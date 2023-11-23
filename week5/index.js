26b194625ef39b49b711940b69214c3b186cf335
Kevin Wilson <79007334+kevinwilson16@users.noreply.github.com>
2023-11-23 22:51:53 +0400

    Implemented code for User Authentication System
    Partly completed the required work for session 5

---
+++ week5/index.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const mysql   = require('mysql');
+const dotenv  = require('dotenv');
+dotenv.config({path: './.env'});
+const path    = require('path');
+const app   = express();
+const port  = 5000;
+
+const db   = mysql.createConnection({
+    host     : process.env.DATABASE_HOST,
+    user    : process.env.DATABASE_USER,
+    password : process.env.DATABASE_PASSWORD,
+    database : process.env.DATABASE
+});
+
+db.connect((error) =>{
+    if(error){
+        console.log(error);
+    }else{
+        console.log("MySQL Connected");
+    }
+})
+
+const publicDirectory = path.join(__dirname, './public');
+app.use(express.static(publicDirectory));
+app.set('view engine', 'hbs');
+
+
+
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+app.get('/register', (req, res) => {
+    res.render('register');
+});
+
+app.listen(5000, () => {
+    console.log(`Server is running on port ${port}`);
+})
+
